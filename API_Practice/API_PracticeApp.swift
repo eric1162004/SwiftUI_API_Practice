6f09cdc1fd94f610acc6e90b7e59bc9e14a03f10
Eric Cheung <eric1162004@hotmail.com>
2022-03-01 11:34:57 -0800

    Initial Commit

---
+++ API_Practice/API_PracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  API_PracticeApp.swift
+//  API_Practice
+//
+//  Created by eric on 2022-03-01.
+//
+
+import SwiftUI
+
+@main
+struct API_PracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
